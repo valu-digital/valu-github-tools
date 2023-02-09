a34359630431d48b454879ba67075112a1a0f722
Joonas Varis <joonas.varis@valu.fi>
2023-02-09 10:14:06 +0200

    Add ts comment

--- src/__tests__/example.test.ts
+++ src/__tests__/example.test.ts
@@ -1,5 +1,7 @@
 test("example test", () => {
     const number = 12;
     const multiplier = "3";
+    // typescript tells you that you shouldn't be
+    // doing arithmetics with strings, neat!
     expect(number * multiplier).toBe(36);
 });
